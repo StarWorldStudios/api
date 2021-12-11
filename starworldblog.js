bc5a9b577275e877a8db1a79eba852771590c4e8
StarWorldStudios <76687040+StarWorldStudios@users.noreply.github.com>
2021-12-11 10:54:25 +0800

    Update starworldblog.js

--- starworldblog.js
+++ starworldblog.js
@@ -1,7 +1,3 @@
 document.body.oncopy = function() {
    swal("复制成功！", "若要转载请保留原文链接，感谢支持！", "success");
 };
-document.getElementByClassName("copy-to-clipboard-button").onclick = function()
-{
-    swal("复制成功！", "若要转载请保留原文链接，感谢支持！", "success");
-};
