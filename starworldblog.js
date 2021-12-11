af7e8a12fa270e23792ec1f39ccb403c9882daa3
StarWorldStudios <76687040+StarWorldStudios@users.noreply.github.com>
2021-12-11 10:48:50 +0800

    Update starworldblog.js

--- starworldblog.js
+++ starworldblog.js
@@ -1,3 +1,3 @@
-$(".copy-to-clipboard-button").click(function(){
-   swal("内容复制成功", "若要转载请务必保留原文链接，申明来源，谢谢合作！", "success");
-});
+document.body.oncopy = function() {
+   swal("复制成功！", "ZGGSONG博客提醒您：若要转载请保留原文链接，感谢支持！", "success");
+};
